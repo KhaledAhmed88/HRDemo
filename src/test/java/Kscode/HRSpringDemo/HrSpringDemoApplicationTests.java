b71d38ef0f8d055c90c5206f261b79be7f4dce73
Khaled <khaled.ahmed94@outlook.com>
2023-06-01 17:58:18 +0300

    test

--- src/test/java/Kscode/HRSpringDemo/HrSpringDemoApplicationTests.java
+++ src/test/java/Kscode/HRSpringDemo/HrSpringDemoApplicationTests.java
@@ -1,13 +1,20 @@
 package Kscode.HRSpringDemo;
 
+import Kscode.HRSpringDemo.entity.Employee;
+import Kscode.HRSpringDemo.service.EmployeeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class HrSpringDemoApplicationTests {
-
+	@Autowired
+	EmployeeService employeeService;
 	@Test
-	void contextLoads() {
+	void findById() {
+		Optional<Employee> employee = employeeService.findById(1);
 	}
 
 }
